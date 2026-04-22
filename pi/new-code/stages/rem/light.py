5ee241bf31e7d412592ab6b4aed8cc85cc990269
Jack-Roddam <roddamjb@outlook.com>
2026-04-22 12:40:53 +0100

    test

--- pi/new-code/stages/rem/light.py
+++ pi/new-code/stages/rem/light.py
@@ -11,53 +11,55 @@ remCycleNo = 3
 async def run(context: dict) -> tuple[str, str, bool]:
     #commented out for testing
     #time.sleep(delayAftrRem)
+
+    def earlyRem():
+        print("executing earlyRem")
+        pixels = neopixel.NeoPixel(board.D18, NoLEDs, brightness=0.01)
+
+        for cycle in range(5):
+            print(cycle)
+            for b in range(0, 100):
+                pixels.brightness = b / 100
+                pixels.fill((255, 0, 0))
+                time.sleep(0.002)
+            for b in range(100, 0, -1):
+                pixels.brightness = b / 100
+                pixels.fill((255, 0, 0))
+                time.sleep(0.002)
+            pixels.fill((0, 0, 0))
+            time.sleep(0.5)
+
+        pixels.fill((0, 0, 0))
+
+    def midNlateRem():
+        print("executing midRem")
+        pixels = neopixel.NeoPixel(board.D18, NoLEDs, brightness=0.01)
+
+        for cycle in range(5):
+                for b in range(0, 100):
+                    pixels.brightness = b / 100
+                    pixels.fill((255, 0, 0))
+                    time.sleep(0.002)
+                for b in range(100, 0, -1):
+                    pixels.brightness = b / 100
+                    pixels.fill((255, 0, 0))
+                    time.sleep(0.002)
+                for b in range(0, 100):
+                    pixels.brightness = b / 100
+                    pixels.fill((255, 60, 0))
+                    time.sleep(0.002)
+                for b in range(100, 0, -1):
+                    pixels.brightness = b / 100
+                    pixels.fill((255, 60, 0))
+                    time.sleep(0.002)
+
+        pixels.fill((0, 0, 0))
+
     print("running")
     if remCycleNo <= 2:
         earlyRem()
     else:
         midNlateRem()
 
+
     return "light cues started", "5 or 10s brust, colour depndent on NremCycle", True
-
-async def earlyRem():
-    print("executing earlyRem")
-    pixels = neopixel.NeoPixel(board.D18, NoLEDs, brightness=0.01)
-
-    for cycle in range(5):
-        print(cycle)
-        for b in range(0, 100):
-            pixels.brightness = b / 100
-            pixels.fill((255, 0, 0))
-            time.sleep(0.002)
-        for b in range(100, 0, -1):
-            pixels.brightness = b / 100
-            pixels.fill((255, 0, 0))
-            time.sleep(0.002)
-        pixels.fill((0, 0, 0))
-        time.sleep(0.5)
-
-    pixels.fill((0, 0, 0))
-
-async def midNlateRem():
-    print("executing midRem")
-    pixels = neopixel.NeoPixel(board.D18, NoLEDs, brightness=0.01)
-
-    for cycle in range(5):
-            for b in range(0, 100):
-                pixels.brightness = b / 100
-                pixels.fill((255, 0, 0))
-                time.sleep(0.002)
-            for b in range(100, 0, -1):
-                pixels.brightness = b / 100
-                pixels.fill((255, 0, 0))
-                time.sleep(0.002)
-            for b in range(0, 100):
-                pixels.brightness = b / 100
-                pixels.fill((255, 60, 0))
-                time.sleep(0.002)
-            for b in range(100, 0, -1):
-                pixels.brightness = b / 100
-                pixels.fill((255, 60, 0))
-                time.sleep(0.002)
-
-    pixels.fill((0, 0, 0))
