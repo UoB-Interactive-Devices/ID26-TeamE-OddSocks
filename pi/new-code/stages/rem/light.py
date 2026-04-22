85ccbfde0acfd9d258c5c5c468c2a90d9782cda1
Jack-Roddam <roddamjb@outlook.com>
2026-04-22 11:10:03 +0100

    test on small strip

--- pi/new-code/stages/rem/light.py
+++ pi/new-code/stages/rem/light.py
@@ -3,16 +3,18 @@ import board
 import neopixel
 
 delayAftrRem = 3 * 60 #3mins
-NoLEDs = 10
+NoLEDs = 3
 
 def run(remCycleNo):
     time.sleep(delayAftrRem)
+    print("running")
     if remCycleNo <= 2:
         earlyRem()
     else:
         midNlateRem()
 
 def earlyRem():
+    print("executing earlyRem")
     pixels = neopixel.NeoPixel(board.D18, NoLEDs, brightness=0.01)
 
     for cycle in range(5):
@@ -31,6 +33,7 @@ def earlyRem():
     pixels.fill((0, 0, 0))
 
 def midNlateRem():
+    print("executing midRem")
     pixels = neopixel.NeoPixel(board.D18, NoLEDs, brightness=0.01)
 
     for cycle in range(5):
